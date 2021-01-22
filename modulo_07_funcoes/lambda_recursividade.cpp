c1f2a26e1be0689ca9025f84b503fa4a6183a87e
Edgard Kuboo <kuboo.dev@gmail.com>
2021-01-22 08:45:08 -0300

    Recursividade em funções lambda

--- modulo_07_funcoes/lambda_recursividade.cpp
+++ modulo_07_funcoes/lambda_recursividade.cpp
@@ -1,10 +1,23 @@
+// Compilar no Linux como:
+//      g++ -std=c++11 lambda_recursividade.cpp -o lambda_recursividade
+
 #include <iostream>
 #include <functional>
 
 int main() {
-    std::function<int (int)> fatorial;
+    // A declaração "auto" para inicialização da variável fatorial não é permitida
+    // por alguns motivos:
+    //   1) A referência da variável "fatorial" passada para a função lambda ocorre antes
+    //      que o tipo de variável seja resolvido pelo compilador
+    //   2) A chamada da variável função "fatorial" em "return" ocorre antes que o seu tipo seja
+    //      resolvido pelo compilador
+    std::function<long int (int)> fatorial;
 
-    fatorial = [&fatorial](int n)->long int {
+    // O nome da função é substituída pela variável de apontamento na função lambda
+    // para que seja possível a recursividade
+    // Por se tratar de uma variável declarada num escopo externo, a mesma deve
+    // ser tratada na função lambda
+    fatorial = [&fatorial](int n) {
         return (n <= 1) ? 1 : n * fatorial(n-1);
     };
 
